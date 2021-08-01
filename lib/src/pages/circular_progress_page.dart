7a690f34fad30d1dad22ee68cc9fb3e63106a622
Oscary <oscary.ramirez@gmail.com>
2021-08-01 19:25:07 -0400

    Animating arco

--- lib/src/pages/circular_progress_page.dart
+++ lib/src/pages/circular_progress_page.dart
@@ -1,4 +1,5 @@
-import 'dart:math';
+import 'dart:math' as Math ;
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
@@ -9,7 +10,26 @@ class CirculaProgressPage extends StatefulWidget {
   _CirculaProgressPageState createState() => _CirculaProgressPageState();
 }
 
-class _CirculaProgressPageState extends State<CirculaProgressPage> {
+class _CirculaProgressPageState extends State<CirculaProgressPage>  with SingleTickerProviderStateMixin{
+  double porcentaje = 0.0;
+  double nuevoPorcentaje=0.0;
+  AnimationController ? controller;
+  @override
+  void initState() {
+    controller = new AnimationController(vsync: this, duration:Duration(milliseconds: 800 ));
+
+    controller?.addListener(() {
+
+      porcentaje = lerpDouble(porcentaje, nuevoPorcentaje, controller!.value) as double;
+      setState(() {});
+    });
+    super.initState();
+  }
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,18 +39,35 @@ class _CirculaProgressPageState extends State<CirculaProgressPage> {
           height: 300.0,
           width: 300.0,
           child: CustomPaint(
-            painter: _MiRadialProgress(),
+            painter: _MiRadialProgress(porcentaje),
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.refresh),
+        backgroundColor: Colors.pink,
+        onPressed: (){
+          porcentaje = nuevoPorcentaje;
+          nuevoPorcentaje += 10;
+          if(porcentaje >100){
+             porcentaje = 0;
+             nuevoPorcentaje=0;
+
+          }
+          controller?.forward(from: 0.0);
+          setState(() {});
+        },
       ),
     );
   }
 }
 
 class _MiRadialProgress extends CustomPainter{
-  @override
+   final porcentaje;
+  _MiRadialProgress(this.porcentaje);
 
-  //Circulo
+
+  @override
   void paint(Canvas canvas, Size size) {
     final paint = Paint()
           ..strokeWidth = 4
@@ -38,7 +75,7 @@ class _MiRadialProgress extends CustomPainter{
           .. style = PaintingStyle.stroke;
 
     final center = Offset(size.width * 0.5, size.height * 0.5);
-    final radio = min( size.width * 0.5, size.height * 0.5) ;
+    final radio = Math.min( size.width * 0.5, size.height * 0.5) ;
     canvas.drawCircle(center, radio, paint);
 
      //Arco
@@ -46,6 +83,15 @@ class _MiRadialProgress extends CustomPainter{
       ..strokeWidth = 10
       ..color = Colors.pink
       .. style = PaintingStyle.stroke;
+
+   //Parte que llena el circulo
+    double arcAngle = 2 * Math.pi *( porcentaje /100);
+     canvas.drawArc(
+         Rect.fromCircle(center: center, radius: radio),
+         -Math.pi/2,
+         arcAngle,
+         false,
+         paintArco);
   }
 
   @override
