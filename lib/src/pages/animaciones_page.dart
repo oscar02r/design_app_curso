8074d5d5fedb805b99aaca38664f2c245a0b526a
Oscary <oscary.ramirez@gmail.com>
2021-08-01 13:08:18 -0400

    Animated multiple option

--- lib/src/pages/animaciones_page.dart
+++ lib/src/pages/animaciones_page.dart
@@ -23,15 +23,53 @@ class CuadradoAnimado  extends StatefulWidget {
 class _CuadradoAnimadoState extends State<CuadradoAnimado> with SingleTickerProviderStateMixin{
   AnimationController ? controller;
   Animation<double> ? rotacion;
-
+  Animation<double> ? opacidad;
+  Animation<double> ? opacidadOut;
+  Animation<double> ? moverDerecha;
+  Animation<double> ? agrandar;
+  
   @override
   void initState() {
-    controller =new  AnimationController(vsync: this, duration: Duration(milliseconds: 4000));
-    rotacion = Tween(begin: 0.0, end: 2 * Math.pi ).animate(controller!);
+    controller = new  AnimationController(vsync: this, duration: Duration(milliseconds: 4000));
+    
+    rotacion = Tween(begin: 0.0, end: 2 * Math.pi ).animate(
+      CurvedAnimation(
+          parent: controller as Animation<double>,
+          curve:Curves.easeOut
+      )
+    );
+
+    opacidad = Tween(begin: 0.1, end: 1.0).animate(
+        CurvedAnimation(
+            parent: controller as Animation<double>,
+            curve:Interval(0.0,0.25, curve: Curves.easeOut)
+        )
+    );
+
+    moverDerecha = Tween(begin: 0.0, end: 200.0).animate(
+        CurvedAnimation(
+            parent: controller as Animation<double>,
+            curve: Curves.easeOut
+        )
+    );
+
+    agrandar = Tween(begin: 0.0, end: 2.0).animate(
+        CurvedAnimation(
+            parent: controller as Animation<double>,
+            curve: Curves.easeOut
+        )
+    );
+    
+    opacidadOut = Tween(begin: 1.0, end:0.0 ).animate(
+        CurvedAnimation(
+            parent: controller as Animation<double>,
+            curve: Interval(0,0.75)
+        )
+    );
 
     controller?.addListener(() {
       if(controller?.status == AnimationStatus.completed){
-        controller?.reverse();
+        controller?.reset();
       }
     });
     super.initState();
@@ -44,12 +82,23 @@ class _CuadradoAnimadoState extends State<CuadradoAnimado> with SingleTickerProv
   @override
   Widget build(BuildContext context) {
     controller?.forward();
+
     return AnimatedBuilder(
-        animation:Listenable.merge([controller]) , 
-        builder: (context, child){
-          return Transform.rotate(
-            angle:  rotacion!.value,
-              child: _Rectagulo()
+        animation:Listenable.merge([controller]) ,
+        child:  _Rectagulo(),
+        builder: (context, childRectangulo){
+          return Transform.translate(
+            offset: Offset(moverDerecha!.value, 0),
+            child: Transform.rotate(
+              angle:  rotacion!.value,
+                child:Opacity(
+                     opacity: opacidad!.value,
+                    child: Transform.scale(
+                        scale: agrandar!.value,
+                        child: childRectangulo
+                    )
+                )
+            ),
           );
         }
     );
